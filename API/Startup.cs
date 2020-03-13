f6a431019eb02fa4a94ec1fb5d08550b628c313e
Richard <richokoye1@gmail.com>
2020-03-13 18:29:39 +0100

    Add MediatR and Activities Entity

--- API/Startup.cs
+++ API/Startup.cs
@@ -12,6 +12,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
+using Application.Activities;
+using MediatR;
 
 namespace API
 {
@@ -32,6 +34,7 @@ namespace API
                     policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
                 }); 
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("Default")));
             services.AddControllers();
         }
